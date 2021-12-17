e43f924c1399885bf506779d39b35c7e1a5835db
luzesquivel <marialuzsandobal@gmail.com>
2021-12-17 15:08:09 -0300

    Cambiamos index

--- funciones.js
+++ funciones.js
@@ -4,15 +4,10 @@ window.onload = iniciar;
 function iniciar() {
    // Se guarda en variable elemento HTML
    var btnCalcular = document.getElementById("calc");
-   
+
    //Para que funcione el evento click (parametro,nombrefuncion)
    btnCalcular.addEventListener("click", clickBtnCalcular);
 
-}
-
-function borrar (){
-   var btnCancelar= document.getElementById("canc");
-   btnCancelar.addEventListener("click", clickBtnCalcular);
 }
 
 var p = document.getElementById("peso");
@@ -20,32 +15,30 @@ var a = document.getElementById("altura");
 
 function clickBtnCalcular() {
    if (p.value != "" && a.value != "") {
-      var imc =(p.value / (a.value * a.value));
-      imc =Math.round(imc);
+      var imc = (p.value / (a.value * a.value));
+      imc = Math.round(imc);
       if (imc < 18) {
-         alert("Su imc es:  " + imc +  ". Su categoria indice: PESO BAJO");
+         alert("Su imc es:  " + imc + ". Su categoria indice: PESO BAJO");
       }
-      else if (imc >=18 && imc <26 ) {
-         alert("Su imc es:  " + imc +  ". Su categoria indice: PESO NORMAL");
+      else if (imc >= 18 && imc < 26) {
+         alert("Su imc es:  " + imc + ". Su categoria indice: PESO NORMAL");
       }
       else if (imc >= 27 && imc < 30) {
-         alert("Su imc es:  " + imc +  " categoria de indice: OBESIDAD GRADO I");
+         alert("Su imc es:  " + imc + " categoria de indice: OBESIDAD GRADO I");
       }
-      else if (imc >= 30 && imc< 40) {
-         alert("Su imc es:  " + imc +   " categoria de indice: OBESIDAD GRADO II");
+      else if (imc >= 30 && imc < 40) {
+         alert("Su imc es:  " + imc + " categoria de indice: OBESIDAD GRADO II");
       }
       else if (imc > 40) {
-         alert("Su imc es:  " + imc +  " categoria de indice: OBESIDAD GRADO III");
+         alert("Su imc es:  " + imc + " categoria de indice: OBESIDAD GRADO III");
       }
    }
    else {
       alert("Ingresa peso y altura por favor");
    }
 
-   function clickBtnCancelar(){
-      alert("Borrado");
-
-   }
-   
 }
+
+
+
 
